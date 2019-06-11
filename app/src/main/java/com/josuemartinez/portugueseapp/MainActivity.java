e442d83ea89c44bc9b5060f8147cd81a64728f4f
josuemnez <josue@josuemartinez.com>
2019-06-11 10:44:40 -0400

    Explicit Intents
    Create explicit intents for Family, Colors, Numbers and Phrases

--- app/src/main/java/com/josuemartinez/portugueseapp/MainActivity.java
+++ app/src/main/java/com/josuemartinez/portugueseapp/MainActivity.java
@@ -1,7 +1,10 @@
 package com.josuemartinez.portugueseapp;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void openNumbersList(View view){
+        Intent i = new Intent(this, NumbersActivity.class);
+        startActivity(i);
+    }
+
+    public void openPhrasesList(View view){
+        Intent i = new Intent(this, PhrasesActivity.class);
+        startActivity(i);
+    }
+    public void openFamilyList(View view){
+        Intent i = new Intent(this, FamilyActivity.class);
+        startActivity(i);
+    }
+    public void openColorsList(View view){
+        Intent i = new Intent(this, ColorsActivity.class);
+        startActivity(i);
     }
 }
