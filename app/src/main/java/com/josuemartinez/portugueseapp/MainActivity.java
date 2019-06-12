d50bf38c3bfc4b3943d7db816b1e1416bfc68cfe
josuemnez <josue@josuemartinez.com>
2019-06-11 20:11:48 -0400

    Creation of clickListener

--- app/src/main/java/com/josuemartinez/portugueseapp/MainActivity.java
+++ app/src/main/java/com/josuemartinez/portugueseapp/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,12 +13,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Find the View that shows the numbers category
+        TextView numbers = (TextView) findViewById(R.id.numbers);
+
+        //Set the clicklistener on that View
+        numbers.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent numbersIntent = new Intent(MainActivity.this, MainActivity.class);
+                //Start new activity
+                startActivity(numbersIntent);
+            }
+        });
     }
 
-    public void openNumbersList(View view){
-        Intent i = new Intent(this, NumbersActivity.class);
-        startActivity(i);
-    }
 
     public void openPhrasesList(View view){
         Intent i = new Intent(this, PhrasesActivity.class);
