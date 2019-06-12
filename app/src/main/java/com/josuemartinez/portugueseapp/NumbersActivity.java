1297f818d950f98e438ca2478c71be05eef7a7db
josuemnez <josue@josuemartinez.com>
2019-06-12 10:51:20 -0400

    ListView and ArrayAdapter

--- app/src/main/java/com/josuemartinez/portugueseapp/NumbersActivity.java
+++ app/src/main/java/com/josuemartinez/portugueseapp/NumbersActivity.java
@@ -2,6 +2,10 @@ package com.josuemartinez.portugueseapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -9,5 +13,25 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        //Create a array of words
+        ArrayList<String> words=new ArrayList<String>();
+        words.add(new String("one"));
+        words.add(new String("two"));
+        words.add(new String("three"));
+        words.add(new String("four"));
+        words.add(new String("five"));
+        words.add(new String("six"));
+        words.add(new String("seven"));
+        words.add(new String("eight"));
+        words.add(new String("nine"));
+        words.add(new String("ten"));
+
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, words);
+
+        ListView listView = (ListView) findViewById(R.id.list);
+
+        listView.setAdapter(itemsAdapter);
+
     }
 }
